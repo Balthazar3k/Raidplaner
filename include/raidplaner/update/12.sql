6c5d2d9a69cbbb468a6557ea11dcfdf81c854117
Balthazar3k <angelo.b3k@gmail.com>
2014-04-18 17:25:19 +0200

    SQL Anpassen

--- include/raidplaner/update/12.sql
+++ include/raidplaner/update/12.sql
@@ -1,10 +1,31 @@
-INSERT INTO `prefix_raid_zeit` (`id`, `zeit`) VALUES
-(1, 'Mo 15:00-16:00'),
-(2, 'Fr 16:00-22:00');
+DROP TABLE `prefix_raid_zeit`, `prefix_raid_zeitgruppen`, `prefix_raid_zeitgruppen_chars`;
 
-CREATE TABLE IF NOT EXISTS `prefix_raid_zeiten` (
-  `id` int(11) NOT NULL auto_increment,
-  `uid` int(11) NOT NULL default 0,
-  `zid` int(11) NOT NULL default 0,
-  PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `prefix_raid_zeit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `weekday` varchar(15) NOT NULL,
+  `start` varchar(5) NOT NULL DEFAULT '00:00',
+  `inv` varchar(5) NOT NULL DEFAULT '00:00',
+  `end` varchar(5) NOT NULL DEFAULT '00:00',
+  `options` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `prefix_raid_zeit_charakter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `zid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `prefix_raid_chars`
+  DROP `s3`,
+  DROP `alter`,
+  DROP `rlname`,
+  DROP `mberuf`,
+  DROP `mskill`,
+  DROP `sberuf`,
+  DROP `sskill`,
+  DROP `pvp`,
+  DROP `raiden`,
+  DROP `punkte`,
+  DROP `teamspeak`;
