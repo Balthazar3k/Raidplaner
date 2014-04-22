5a234d0528dad3fe413ab00b246b7c2b9687b524
Balthazar3k <angelo.b3k@gmail.com>
2014-04-22 19:21:33 +0200

    Update Database

---
+++ include/raidplaner/update/13.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `prefix_raid_raid` 
+    DROP `stammgrp`, 
+    DROP `treff`,
+    DROP `invsperre`,
+    CHANGE `statusmsg` `status` INT(11) NOT NULL,
+    CHANGE `erstellt` `created` TIMESTAMP NOT NULL, 
+    CHANGE `bosskey` `series` INT(11) NOT NULL,
+    CHANGE `von` `creator` INT(11) NULL DEFAULT NULL,
+    CHANGE `gruppen` `group` INT(11) NOT NULL,
+    CHANGE `inzen` `dungeon` INT(11) NOT NULL,
+    ADD `updated` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `txt`,
+    ADD `title` VARCHAR(128) NOT NULL AFTER `group`,
+    ADD `weekdays` TEXT NOT NULL ;
