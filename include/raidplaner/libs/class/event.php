5bcc59f9f50a6ea4ba1a2282b6fb8baa773f5854
Balthazar3k <angelo.b3k@gmail.com>
2014-04-22 19:56:00 +0200

    event()->get() & event()->standart_sql();

--- include/raidplaner/libs/class/event.php
+++ include/raidplaner/libs/class/event.php
@@ -10,8 +10,28 @@ class Event {
         return $this;
     }
     
-    private function db(){
-        return $this->raidplaner->db();
+    private function db($table = NULL){
+        return $this->raidplaner->db($table);
+    }
+    
+    private function standart_sql(){
+        return "
+            SELECT
+                a.id, a.title, a.txt, a.series, a.weekdays, a.inv, a.pull, a.end, a.updated, a.created, a.creator,
+                b.id AS status_id, b.statusmsg AS status_name, b.color AS status_color,
+                c.id AS leader_id, c.name AS leader_name,
+                d.id AS group_id, d.gruppen AS group_name, d.regeln AS group_rules,
+                e.id AS dungeon_id, e.small AS dungeon_small, e.name AS dungeon_name, e.grpsize AS dungeon_size,
+                f.id AS loot_id, f.loot AS loot_name,
+                g.id AS creator_id, g.name AS creator_name
+            FROM prefix_raid_raid AS a
+                LEFT JOIN prefix_raid_statusmsg AS b ON b.id = a.status
+                LEFT JOIN prefix_raid_chars AS c ON c.id = a.leader
+                LEFT JOIN prefix_raid_gruppen AS d ON d.id = a.group
+                LEFT JOIN prefix_raid_inzen AS e ON e.id = a.dungeon
+                LEFT JOIN prefix_raid_loot AS f ON f.id = a.loot
+                LEFT JOIN prefix_raid_chars AS g ON g.id = a.creator
+        ";
     }
 
     public function setId($id){
@@ -24,14 +44,21 @@ class Event {
     }
     
     public function get(){
-        
+        if( $this->_id ){
+            return $this->db()->queryRow(
+                $this->standart_sql()."
+                WHERE a.id = '".$this->_id."'    
+            ");     
+        } else {
+            trigger_error('no Event ID');
+        }
     }
     
     public function delete(){
         
     }
     
-    public function list(){
+    public function getList(){
         
     }
     
--- include/raidplaner/update/13.sql
+++ include/raidplaner/update/13.sql
@@ -5,9 +5,10 @@ ALTER TABLE `prefix_raid_raid`
     CHANGE `statusmsg` `status` INT(11) NOT NULL,
     CHANGE `erstellt` `created` TIMESTAMP NOT NULL, 
     CHANGE `bosskey` `series` INT(11) NOT NULL,
-    CHANGE `von` `creator` INT(11) NULL DEFAULT NULL,
+    CHANGE `von` `creator` INT(11) NOT NULL,
     CHANGE `gruppen` `group` INT(11) NOT NULL,
     CHANGE `inzen` `dungeon` INT(11) NOT NULL,
+    CHANGE `ende` `end` INT(11) NOT NULL,
     ADD `updated` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `txt`,
     ADD `title` VARCHAR(128) NOT NULL AFTER `group`,
     ADD `weekdays` TEXT NOT NULL ;
